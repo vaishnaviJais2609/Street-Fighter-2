ddd2b6d373540ff2ca84ac3e24c226b46d07e281
tusharTalan1 <tushartalan10@gmail.com>
2026-09-08 15:37:37 +0530

    feat: add gravity + ground collisionto the fighter

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext('2d');
 canvas.width = 1024;
 canvas.height = 576;
 
+const gravity = 0.7;
+
 class Fighter {
     constructor(x, y, color) {
         this.x = x;
@@ -11,6 +13,7 @@ class Fighter {
         this.width = 50;
         this.height = 150;
         this.color = color;
+        this.velocityY = 0;
     }
 
     draw() {
@@ -20,11 +23,20 @@ class Fighter {
 
     update() {
         this.draw();
+        
+        this.y += this.velocityY;
+
+        if (this.y + this.height + this.velocityY >= canvas.height) {
+            this.velocityY = 0;
+            this.y = canvas.height - this.height;
+        } else {
+            this.velocityY += gravity;
+        }
     }
 }
 
-const player1 = new Fighter(200, canvas.height - 150, 'red');
-const player2 = new Fighter(canvas.width - 250, canvas.height - 150, 'blue');
+const player1 = new Fighter(200, 0, 'red');
+const player2 = new Fighter(canvas.width - 250, 0, 'blue');
 
 
 function gameLoop() {
